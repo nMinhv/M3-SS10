bee6e216276535a089f79c0995008436c1f5f3d5
nMinhv <aquarius.mizu0902@gmail.com>
2023-10-03 08:13:56 +0700

    update

--- src/ra/bt/free/run/StudentManagement.java
+++ src/ra/bt/free/run/StudentManagement.java
@@ -5,11 +5,12 @@ import ra.bt.free.Student;
 import java.util.*;
 
 public class StudentManagement {
+    static ArrayList<Student> studentList = new ArrayList<Student>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Collection<Student> studentList = new ArrayList<Student>();
-        studentList.add(new Student(1,"a","a","a",1,true));
-        studentList.add(new Student(2,"b","b","b",2,false));
+        studentList.add(new Student(1, "a", "a", "a", 1, true));
+        studentList.add(new Student(2, "b", "b", "b", 2, false));
         while (true) {
             System.out.println("********** StudentManagement************ ");
             System.out.println("1:Thêm mới học sinh");
@@ -29,25 +30,38 @@ public class StudentManagement {
                     studentList.add(newS);
                     break;
                 case 2:
+                    updateStudent(scanner);
                     break;
                 case 3:
+                    removeStudent(scanner);
                     break;
                 case 4:
+                    searchStudent(scanner);
                     break;
                 case 5:
                     break;
                 case 6:
-                    for (Student data: studentList
-                         ) {
+                    for (Student data : studentList
+                    ) {
                         data.display();
                     }
                     break;
                 case 7:
+                    Collections.sort(studentList, new Comparator<Student>() {
+                        @Override
+                        public int compare(Student student1, Student student2) {
+                            return Double.compare(student1.getMark(), student2.getMark());
+                        }
+                    });
                     break;
                 case 8:
-
+                    Collections.sort(studentList, new Comparator<Student>() {
+                        @Override
+                        public int compare(Student student1, Student student2) {
+                            return Double.compare(student2.getMark(), student1.getMark());
+                        }
+                    });
                     break;
-
                 case 9:
                     scanner.close();
                     System.exit(0);
@@ -55,6 +69,76 @@ public class StudentManagement {
                 default:
                     break;
             }
+        }
+    }
+    public static void updateStudent(Scanner sc) {
+        System.out.println("Nhập Id sinh viên cần sửa");
+        int idUpdate = Integer.parseInt(sc.nextLine());
+        int index = -1;
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getStudentId() == idUpdate) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            System.out.println("Mã học sinh không tồn tại");
+        } else {
+            Student updateStudent = studentList.get(index);
+
+            System.out.println("Nhập Tên học viên");
+            updateStudent.setName(sc.nextLine());
+
+            System.out.println("Nhập địa chỉ");
+            updateStudent.setAddress(sc.nextLine());
+
+            System.out.println("Nhập số điện thoại");
+            updateStudent.setPhone(sc.nextLine());
+
+            System.out.println("Nhập giới tính");
+            updateStudent.setSex(Boolean.parseBoolean(sc.nextLine()));
+
+            System.out.println("Nhập điểm");
+            updateStudent.setMark(Double.parseDouble(sc.nextLine()));
+
+            studentList.set(index, updateStudent);
+            System.out.println("Bạn đã đổi thông tin thành công");
+        }
+    }
+
+    // 3. Xoá học sinh
+    public static void removeStudent(Scanner scanner) {
+        System.out.println("Nhập Id sinh viên cần xoá");
+        int idRemove = Integer.parseInt(scanner.nextLine());
+        int index = -1;
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getStudentId() == idRemove) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            System.out.println("Mã học sinh cần xoá không tìm thấy");
+        } else {
+            studentList.remove(index);
+        }
+    }
+
+    // Tìm kiếm theo Id
+    public static void searchStudent(Scanner sc) {
+        System.out.println("Nhập Id sinh viên cần tìm kiếm");
+        int idSearch = Integer.parseInt(sc.nextLine());
+        int index = -1;
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getStudentId() == idSearch) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            System.out.println("Học sinh không tìm thấy");
+        } else {
+            studentList.get(index).display();
         }
     }
 
